61753a84889da1d27d59501d296e0d34d57634ab
joshjun0122 <josh989870@gmail.com>
2024-12-15 23:16:58 +0800

    first commit

---
+++ P18/work/work/work.cpp
@@ -0,0 +1,23 @@
+﻿/*使用fread()函數讀取檔案內容*/
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 80
+
+int main(void)
+{
+    FILE* in, * out;
+    int Cnt;
+    char str[100];
+    in = fopen("welcome.txt", "r");
+    out = fopen("output.txt", "w");
+    while (!feof(in))
+    {
+        Cnt = fscanf(in, "%s", str);//讀取檔案
+        if (Cnt > 0)
+            fprintf(out, "%s\n", str);//寫入檔案
+    }
+    fclose(in);        //關閉檔案
+    fclose(out);       //關閉檔案
+    system("pause");
+    return 0;
+}
